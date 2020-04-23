c18affb538fb2fb4b4a3ba226b966981a0be40d5
Stephen Ash <stephenash@gmail.com>
2020-04-23 09:55:18 -0700

    ci: set repo name in deployment script

--- ci/travis-ci-deploy.sh
+++ ci/travis-ci-deploy.sh
@@ -1,10 +1,12 @@
+export PREBUILD_SLUG=$TRAVIS_REPO_SLUG
+
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker push $DOCKER_TAG
   docker run \
     --env CANVAS_PREBUILT_VERSION \
     --env PREBUILD_AUTH \
-    --env PREBUILD_SLUG=$TRAVIS_REPO_SLUG \
+    --env PREBUILD_SLUG \
     --volume "$(pwd):/build" $DOCKER_TAG \
     bash -c 'cd /build; export NVM_DIR=$HOME/.nvm; . $HOME/.nvm/nvm.sh; . ci/release.sh linux "$CANVAS_PREBUILT_VERSION"'
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
