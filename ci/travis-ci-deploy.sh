6bc2c7cbd41e981c0e19a58aefe3a05a00f771b4
Stephen Ash <stephenash@gmail.com>
2020-04-22 18:50:16 -0700

    Squashed commit of the following:
    commit 612c3d6f31060d88facf8b39b8845533a148c82c
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 18:49:45 2020 -0700
    
        travis ci testing
    
    commit fd67598537fd9356db3c5b693eb4b132a81db5cb
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 18:48:30 2020 -0700
    
        travis ci testing
    
    commit c309e761f84cba7cfdd2e822220ded81549c0599
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 18:33:57 2020 -0700
    
        travis ci testing
    
    commit 2682e8f63e7e730c2fb0e447377a00ef497a2b70
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 18:30:55 2020 -0700
    
        travis ci testing
    
    commit 08fc17e0c3467102c5ef4c42731137bd94e2ba19
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 18:22:02 2020 -0700
    
        travis ci testing
    
    commit 45442811aa4c76961fd73171ccba02b7b24d4264
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 18:20:44 2020 -0700
    
        travis ci testing
    
    commit 4ac7e77071b745a0c06dd893cea462f7c5eccf94
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 18:15:09 2020 -0700
    
        travis ci testing
    
    commit 234b31b25c1723e3783d85d8cb558732c3bb2380
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 18:11:52 2020 -0700
    
        travis ci testing
    
    commit c4d43602f53327a1ee7690c90736a214b5c3e05e
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 18:05:38 2020 -0700
    
        travis ci testing
    
    commit 9b5313f90920c8383002db5161fb55d4badec418
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 18:02:37 2020 -0700
    
        travis ci testing
    
    commit 4fd857e5e2700a257954f593fefc4c240c7f5548
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 18:00:56 2020 -0700
    
        travis ci testing
    
    commit f4a1296519317e1ae2deb4dc5b2852e53b5fd174
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 17:57:36 2020 -0700
    
        travis ci testing
    
    commit 59b5c0d5e86c381c29a22fd70b42b72810ec9d78
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 17:46:27 2020 -0700
    
        travis ci testing
    
    commit 9d0d13f94ef9d8c901fa870e4f2faf97bd26c36e
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 17:41:11 2020 -0700
    
        travis ci testing
    
    commit 105f3228def57647cbb2860f9565a4b2ea2ad3a4
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 17:37:56 2020 -0700
    
        travis ci testing
    
    commit c17740a66e92718d499087fa019b663c4346acf7
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 17:34:46 2020 -0700
    
        travis ci testing
    
    commit 7e4e8cde17120060f0b7abed88d95c532c4c7f8d
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 17:32:20 2020 -0700
    
        travis ci testing
    
    commit 259b668c76f18fee376bc0b241d0daf4ce7a132f
    Author: Stephen Ash <stephenash@gmail.com>
    Date:   Wed Apr 22 17:24:58 2020 -0700
    
        travis ci testing

---
+++ ci/travis-ci-deploy.sh
@@ -0,0 +1,12 @@
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  docker push $DOCKER_TAG
+  docker run \
+    --env CANVAS_PREBUILT_VERSION \
+    --env PREBUILD_AUTH \
+    --env PREBUILD_SLUG=$TRAVIS_REPO_SLUG \
+    --volume "$(pwd):/build" $DOCKER_TAG \
+    bash -c 'cd /build; export NVM_DIR=$HOME/.nvm; . $HOME/.nvm/nvm.sh; . ci/release.sh linux "$CANVAS_PREBUILT_VERSION"'
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  bash ci/release.sh osx "$CANVAS_PREBUILT_VERSION";
+fi
